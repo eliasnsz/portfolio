6ca50a40f6190e633c756878ceb86892e3fbeb20
Elias Souza <eliasnsouza1245@gmail.com>
2023-11-21 17:25:44 -0300

    footer responsive fix

--- app/page.tsx
+++ app/page.tsx
@@ -34,29 +34,28 @@ export default function Home() {
       </Container>
 
       <Container className="mb-[4em]">
-        <footer className="border text-base text-stone-500 border-stone-600 flex items-center divide-x divide-stone-600 justify-between">
-          <div className="p-6 flex-1 text-center">
-            <p>&copy; Todos os direitos reservados.</p>
+        <footer className="lg:border grid text-base text-stone-400 border-stone-400 lg:divide-stone-400 grid-rows-[min-content_min-content_min-content] lg:grid-cols-[2fr_3fr_2fr] lg:divide-x">
+          <div className="p-6 lg:order-1 text-center order-3">
+            &copy; Todos os direitos reservados.
           </div>
-          <div className="p-6 flex-1 text-center flex items-center gap-3 justify-center">
-            <div className="h-2 w-2 rounded-full flex items-center justify-center bg-green-500">
-              <div className="h-3 w-3 aspect-square rounded-full bg-green-500 animate-ping" />
+          <div className="p-6 order-1 lg:order-2 text-center flex items-center justify-center gap-3">
+            <div className="h-2 flex items-center justify-center aspect-square w-2 rounded-full bg-green-500">
+              <div className="h-4 w-4 animate-ping rounded-full aspect-square bg-green-600/50" />
             </div>
-
-            <a
-              href="#"
-              className="text-stone-300 underline hover:text-stone-200 transition-colors"
-            >
+            <a href="#" className="text-stone-300 underline">
               Disponível para novos projetos.
             </a>
           </div>
-          <div className="p-6 flex gap-4 justify-center flex-1 text-center">
+          <div className="p-6 lg:order-3 order-2 text-center flex items-center justify-center gap-4">
             {socials.map(({ href, icon: Icon }) => (
-              <a href={href} target="_blank" rel="noreferer" key={Icon}>
-                <Icon
-                  size={20}
-                  className="text-stone-400 hover:text-stone-300 transition-colors"
-                />
+              <a
+                href={href}
+                className="transition-colors hover:text-stone-200"
+                key={href}
+                target="_blank"
+                rel="noreferer"
+              >
+                <Icon size={20} />
               </a>
             ))}
           </div>
